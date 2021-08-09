ed35639dda2930dbf7984f50c7c70801af52ddd3
Sanketh Vedula <sankethsai97@gmail.com>
2021-08-09 08:41:19 +0530

    Pass the desired alpha_low and alpha_high instead of hardcoding. Remove T**d as it is now part of the API.

--- experiments/cvr.py
+++ experiments/cvr.py
@@ -1,6 +1,7 @@
+from math import sqrt
 from typing import Dict
 
-from numpy import array
+from numpy import array, argsort
 from numpy.linalg import pinv
 
 from vqr import VectorQuantileEstimator
@@ -9,12 +10,16 @@ from vqr.data import split_train_calib_test, generate_linear_x_y_mvn_data
 from vqr.coverage import measure_width, measure_coverage
 
 
-def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
+def experiment(
+    X_: array, Y_: array, alpha_low_: float, alpha_high_: float, n_levels: int
+) -> Dict[str, float]:
     assert X_.shape[0] == Y_.shape[0]
-    d = Y_.shape[1]
+    assert alpha_low_ < alpha_high_
+    assert 0 <= alpha_low_ <= 1
+    assert 0 <= alpha_high_ <= 1
 
     # split train-valid-test
-    splits = split_train_calib_test(X_, Y_, split_ratios=(0.35, 0.65))
+    splits = split_train_calib_test(X_, Y_, split_ratios=(0.15, 0.65))
 
     # Fit a linear model on train set
     X_train, Y_train = splits["train"]
@@ -28,21 +33,28 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
     vq = VectorQuantileEstimator(n_levels)
     fitted_vq = vq.fit(scores_calib)
     VQ1, VQ2 = fitted_vq.quantile_values
-    VQ1 = VQ1.reshape([n_levels, n_levels])
-    VQ2 = VQ2.reshape([n_levels, n_levels])
+    T = fitted_vq.quantile_levels
 
-    # Construct a quantile vq_surface
-    # When t = 40, 1:-2 is equivalent to asking 0.05 - 0.95.
-    # Thus we expect 90% coverage
-    vq_surface = (
-        array(
+    # Construct a quantile contour
+    idx_low = argsort(abs(T - alpha_low_))[0]
+    idx_high = argsort(abs(T - alpha_high_))[0]
+
+    vq_contour = array(
+        [
             [
-                [*VQ1[1:-2, -2], *VQ1[1, 1:-2], *VQ1[1:-2, 1], *VQ1[-2, 1:-2]],
-                [*VQ2[1:-2, -2], *VQ2[1, 1:-2], *VQ2[1:-2, 1], *VQ2[-2, 1:-2]],
-            ]
-        ).T
-        * (n_levels ** d)
-    )
+                *VQ1[idx_low : idx_high + 1, idx_high + 1],
+                *VQ1[idx_low, idx_low : idx_high + 1],
+                *VQ1[idx_low : idx_high + 1, idx_low],
+                *VQ1[idx_high + 1, idx_low : idx_high + 1],
+            ],
+            [
+                *VQ2[idx_low : idx_high + 1, idx_high + 1],
+                *VQ2[idx_low, idx_low : idx_high + 1],
+                *VQ2[idx_low : idx_high + 1, idx_low],
+                *VQ2[idx_high + 1, idx_low : idx_high + 1],
+            ],
+        ]
+    ).T
 
     # Fit scalar quantiles
     sq = ScalarQuantileEstimator(n_levels)
@@ -50,19 +62,19 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
     SQ = fitted_sq.quantile_values
     SQ1 = SQ[:, 0]
     SQ2 = SQ[:, 1]
-    sq_surface = array(
+    sq_contour = array(
         [
             [
-                *SQ1[1:-1],
-                *SQ1[1:-1],
-                *[SQ1[1]] * len(SQ2[1:-1]),
-                *[SQ1[-1]] * len(SQ2[1:-1]),
+                *SQ1[idx_low : idx_high + 1],
+                *SQ1[idx_low : idx_high + 1],
+                *[SQ1[idx_low]] * len(SQ2[idx_low : idx_high + 1]),
+                *[SQ1[idx_high + 1]] * len(SQ2[idx_low : idx_high + 1]),
             ],
             [
-                *[SQ2[-1]] * len(SQ1[1:-1]),
-                *[SQ2[1]] * len(SQ1[1:-1]),
-                *SQ2[1:-1],
-                *SQ2[1:-1],
+                *[SQ2[idx_high + 1]] * len(SQ1[idx_low : idx_high + 1]),
+                *[SQ2[idx_low]] * len(SQ1[idx_low : idx_high + 1]),
+                *SQ2[idx_low : idx_high + 1],
+                *SQ2[idx_low : idx_high + 1],
             ],
         ]
     ).T
@@ -74,15 +86,15 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
     sq_coverages_ = []
     for i in range(X_test.shape[0]):
         # prediction set for the data point i
-        prediction_set_vq = vq_surface + X_test[i, :] @ A_hat
-        prediction_set_sq = sq_surface + X_test[i, :] @ A_hat
+        prediction_set_vq = vq_contour + X_test[i, :] @ A_hat
+        prediction_set_sq = sq_contour + X_test[i, :] @ A_hat
 
         # measure coverage (0/1) for point i
         vq_coverages_.append(measure_coverage(prediction_set_vq, Y_test[i, :][None, :]))
         sq_coverages_.append(measure_coverage(prediction_set_sq, Y_test[i, :][None, :]))
 
-    vq_width_ = measure_width(vq_surface)
-    sq_width_ = measure_width(sq_surface)
+    vq_width_ = measure_width(vq_contour)
+    sq_width_ = measure_width(sq_contour)
     vq_coverage_ = sum(vq_coverages_) / len(vq_coverages_)
     sq_coverage_ = sum(sq_coverages_) / len(sq_coverages_)
 
@@ -91,17 +103,24 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
         "sq_width": sq_width_,
         "vq_coverage": vq_coverage_,
         "sq_coverage": sq_coverage_,
+        "realized_alpha_low": T[idx_low],
+        "realized_alpha_high": T[idx_high],
+        "deflated_alpha_low": T[idx_low],
+        "deflated_alpha_high": T[idx_high],
     }
 
     return results_
 
 
 if __name__ == "__main__":
-    num_trials = 10
-    n = 2000
+    num_trials = 100
+    n = 1500
     d = 2
-    k = 5
-    X, Y = generate_linear_x_y_mvn_data(n=n, d=d, k=k)
+    k = 4
+    alpha_low = 0.05
+    alpha_high = 0.95
+    n_levels = 40
+    X, Y = generate_linear_x_y_mvn_data(n=n, d=d, k=k, seed=42)
 
     vq_coverages = []
     vq_widths = []
@@ -109,7 +128,7 @@ if __name__ == "__main__":
     sq_coverages = []
     sq_widths = []
     for j in range(num_trials):
-        results = experiment(X, Y, n_levels=40)
+        results = experiment(X, Y, alpha_low, alpha_high, n_levels)
         vq_coverages.append(results["vq_coverage"])
         vq_widths.append(results["vq_width"])
 
@@ -118,10 +137,14 @@ if __name__ == "__main__":
 
         print(
             f"Trial {j}: \n"
-            f"Vector quantiles: coverage={results['vq_coverage']}%, "
-            f"width={results['vq_width']} \n"
-            f"Scalar quantiles: coverage={results['sq_coverage']}%, "
-            f"width={results['sq_width']} \n"
+            f"Coverage level: [{round(results['realized_alpha_low'], 3)}, "
+            f"{round(results['realized_alpha_high'], 3)}] \n"
+            f"Vector quantiles: coverage={round(results['vq_coverage'], 3)}%, "
+            f"width={round(results['vq_width'], 3)} \n"
+            f"Deflated coverage level: [{round(results['deflated_alpha_low'], 3)}, "
+            f"{round(results['deflated_alpha_high'], 3)}] \n"
+            f"Scalar quantiles: coverage={round(results['sq_coverage'], 3)}%, "
+            f"width={round(results['sq_width'], 3)} \n"
         )
 
     marginal_vq_coverage = sum(vq_coverages) / len(vq_coverages)
@@ -140,3 +163,7 @@ if __name__ == "__main__":
         f"Marginal coverage over {num_trials} trials: "
         f"{marginal_sq_coverage}, average width = {average_sq_width}."
     )
+    print(vq_coverages)
+    print(vq_widths)
+    print(sq_coverages)
+    print(sq_widths)
--- experiments/vqe.py
+++ experiments/vqe.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from numpy import array
+from numpy import array, argsort
 
 from vqr.api import ScalarQuantileEstimator, VectorQuantileEstimator
 from vqr.data import (
@@ -12,7 +12,9 @@ from vqr.data import (
 from vqr.coverage import measure_width, measure_coverage
 
 
-def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
+def experiment(
+    X_: array, Y_: array, alpha_low_: float, alpha_high_: float, n_levels: int
+) -> Dict[str, float]:
     assert X_.shape[0] == Y_.shape[0]
     d = Y_.shape[1]
 
@@ -28,17 +30,28 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
 
     # Get the quantiles
     VQ1, VQ2 = fitted_vq.quantile_values
+    T = fitted_vq.quantile_levels
 
-    # Construct a quantile surface
-    vq_surface = (
-        array(
+    # Construct a quantile contour
+    idx_low = argsort(abs(T - alpha_low_))[0]
+    idx_high = argsort(abs(T - alpha_high_))[0]
+
+    vq_contour = array(
+        [
             [
-                [*VQ1[1:-2, -2], *VQ1[1, 1:-2], *VQ1[1:-2, 1], *VQ1[-2, 1:-2]],
-                [*VQ2[1:-2, -2], *VQ2[1, 1:-2], *VQ2[1:-2, 1], *VQ2[-2, 1:-2]],
-            ]
-        ).T
-        * (n_levels ** d)
-    )
+                *VQ1[idx_low : idx_high + 1, idx_high + 1],
+                *VQ1[idx_low, idx_low : idx_high + 1],
+                *VQ1[idx_low : idx_high + 1, idx_low],
+                *VQ1[idx_high + 1, idx_low : idx_high + 1],
+            ],
+            [
+                *VQ2[idx_low : idx_high + 1, idx_high + 1],
+                *VQ2[idx_low, idx_low : idx_high + 1],
+                *VQ2[idx_low : idx_high + 1, idx_low],
+                *VQ2[idx_high + 1, idx_low : idx_high + 1],
+            ],
+        ]
+    ).T
 
     # Fit scalar quantiles
     sq = ScalarQuantileEstimator(n_levels)
@@ -46,19 +59,19 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
     SQ = fitted_sq.quantile_values
     SQ1 = SQ[:, 0]
     SQ2 = SQ[:, 1]
-    sq_surface = array(
+    sq_contour = array(
         [
             [
-                *SQ1[1:-1],
-                *SQ1[1:-1],
-                *[SQ1[1]] * len(SQ2[1:-1]),
-                *[SQ1[-1]] * len(SQ2[1:-1]),
+                *SQ1[idx_low : idx_high + 1],
+                *SQ1[idx_low : idx_high + 1],
+                *[SQ1[idx_low]] * len(SQ2[idx_low : idx_high + 1]),
+                *[SQ1[idx_high + 1]] * len(SQ2[idx_low : idx_high + 1]),
             ],
             [
-                *[SQ2[-1]] * len(SQ1[1:-1]),
-                *[SQ2[1]] * len(SQ1[1:-1]),
-                *SQ2[1:-1],
-                *SQ2[1:-1],
+                *[SQ2[idx_high + 1]] * len(SQ1[idx_low : idx_high + 1]),
+                *[SQ2[idx_low]] * len(SQ1[idx_low : idx_high + 1]),
+                *SQ2[idx_low : idx_high + 1],
+                *SQ2[idx_low : idx_high + 1],
             ],
         ]
     ).T
@@ -67,18 +80,22 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
     _, Y_test = datasets["test"]
 
     # Vector quantile: coverage and width
-    vq_coverage_ = measure_coverage(vq_surface, Y_test)
-    vq_width_ = measure_width(vq_surface)
+    vq_coverage_ = measure_coverage(vq_contour, Y_test)
+    vq_width_ = measure_width(vq_contour)
 
     # Scalar quantile: coverage and width
-    sq_coverage_ = measure_coverage(sq_surface, Y_test)
-    sq_width_ = measure_width(sq_surface)
+    sq_coverage_ = measure_coverage(sq_contour, Y_test)
+    sq_width_ = measure_width(sq_contour)
 
     results_ = {
         "sq_width": sq_width_,
         "sq_coverage": sq_coverage_,
         "vq_width": vq_width_,
         "vq_coverage": vq_coverage_,
+        "realized_alpha_low": T[idx_low],
+        "realized_alpha_high": T[idx_high],
+        "deflated_alpha_low": T[idx_low],
+        "deflated_alpha_high": T[idx_high],
     }
 
     return results_
@@ -86,9 +103,11 @@ def experiment(X_: array, Y_: array, n_levels: int) -> Dict[str, float]:
 
 if __name__ == "__main__":
     dataset_name = "heart"
-    num_trials = 10
-    n = 2000
+    num_trials = 50
+    n = 1500
     d_ = 2
+    alpha_low = 0.05
+    alpha_high = 0.95
 
     # Generate data
     if dataset_name == "mvn":
@@ -98,6 +117,7 @@ if __name__ == "__main__":
     elif dataset_name == "star":
         X, Y = generate_star()
     else:
+        X, Y = None, None
         NotImplementedError(f"Unrecognized {dataset_name=}")
 
     vq_coverages = []
@@ -106,8 +126,8 @@ if __name__ == "__main__":
     sq_coverages = []
     sq_widths = []
 
-    for i in range(num_trials):
-        results = experiment(X, Y, n_levels=40)
+    for j in range(num_trials):
+        results = experiment(X, Y, alpha_low, alpha_high, n_levels=40)
         vq_coverages.append(results["vq_coverage"])
         vq_widths.append(results["vq_width"])
 
@@ -115,11 +135,15 @@ if __name__ == "__main__":
         sq_widths.append(results["sq_width"])
 
         print(
-            f"Trial {i}: \n"
-            f"Vector quantiles: coverage={results['vq_coverage']}%, "
-            f"width={results['vq_width']} \n"
-            f"Scalar quantiles: coverage={results['sq_coverage']}%, "
-            f"width={results['sq_width']} \n"
+            f"Trial {j}: \n"
+            f"Coverage level: [{round(results['realized_alpha_low'], 3)}, "
+            f"{round(results['realized_alpha_high'], 3)}] \n"
+            f"Vector quantiles: coverage={round(results['vq_coverage'], 3)}%, "
+            f"width={round(results['vq_width'], 3)} \n"
+            f"Deflated coverage level: [{round(results['deflated_alpha_low'], 3)}, "
+            f"{round(results['deflated_alpha_high'], 3)}] \n"
+            f"Scalar quantiles: coverage={round(results['sq_coverage'], 3)}%, "
+            f"width={round(results['sq_width'], 3)} \n"
         )
 
     marginal_vq_coverage = sum(vq_coverages) / len(vq_coverages)
@@ -138,3 +162,7 @@ if __name__ == "__main__":
         f"Marginal coverage over {num_trials} trials: "
         f"{marginal_sq_coverage}, average width = {average_sq_width}."
     )
+    print(vq_coverages)
+    print(vq_widths)
+    print(sq_coverages)
+    print(sq_widths)
