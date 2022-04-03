ecac4521dca531a65b01b2d5e116a9dac42e8611
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-04-03 16:47:53 +0300

    experiments: out dir

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,4 +1,10 @@
 import os
+from pathlib import Path
+
+EXPERIMENTS_ROOT_DIR = Path(__file__).parent
+
+EXPERIMENTS_OUT_DIR = EXPERIMENTS_ROOT_DIR.parent.joinpath("out")
+os.makedirs(EXPERIMENTS_OUT_DIR, exist_ok=True)
 
 # Fix CPATH for KeOps
 conda_prefix = os.environ.get("CONDA_PREFIX")
