6ae2c94247405567cc19a6713a72b2960233b282
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-04-03 23:31:13 +0300

    setup_logging: support level flag as override to yaml

--- experiments/logging/__init__.py
+++ experiments/logging/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+from typing import Optional
 from pathlib import Path
 
 import yaml
@@ -7,8 +8,12 @@ import yaml
 LOGGING_CONFIG = Path(__file__).parent.joinpath("logging.yaml")
 
 
-def setup_logging():
+def setup_logging(level: Optional[int] = None):
     with open(LOGGING_CONFIG, "r") as f:
         logging_config = yaml.safe_load(f)
+
+    if level is not None:
+        for logger_name, logger_settings in logging_config["loggers"].items():
+            logger_settings["level"] = logging.getLevelName(level)
 
     logging.config.dictConfig(logging_config)
