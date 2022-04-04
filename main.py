0608fae752671a9c8b4d10d017f8403cfe8cbcf0
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-04-05 00:51:13 +0300

    main: global CLI args

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
+from pathlib import Path
 
 import click
 
+from experiments import EXPERIMENTS_OUT_DIR
 from experiments.scale import run_scale_exps
 from experiments.logging import setup_logging
 
@@ -9,8 +11,20 @@ _LOG = logging.getLogger(__name__)
 
 
 @click.group(context_settings=dict(show_default=True))
+@click.pass_context
 @click.option("--debug/--no-debug", default=False, help="Use DEBUG logging")
-def main(debug):
+@click.option("-g", "--gpu/--no-gpu", default=False, help="Enable GPU support")
+@click.option("--devices", default=None, type=str, help="GPU devices (comma separated)")
+@click.option("-p", "--processes", type=int, default=1, help="Number of workers")
+@click.option("-o", "--out-dir", type=Path, default=EXPERIMENTS_OUT_DIR, help="Out dir")
+def main(
+    ctx: click.Context,
+    debug: bool,
+    gpu: bool,
+    devices: str,
+    processes,
+    out_dir: Path,
+):
     setup_logging(level=logging.DEBUG if debug else logging.INFO)
 
 
