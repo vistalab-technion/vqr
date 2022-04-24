776c5d5df49b0750d2261a1393e10305c4e7e854
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-04-24 12:09:55 +0300

    QuantileFunctionDataProviderWrapper: tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,7 @@ setup_logging()
 def test_out_dir(request: FixtureRequest) -> Path:
     """
     Creates a unique output directory in which a test can write some results.
+    It will be cleared if it existed before.
 
     The directory will live under :obj:`TESTS_OUT_DIR` and be named according to the
     test class and test method.
@@ -27,15 +28,29 @@ def test_out_dir(request: FixtureRequest) -> Path:
 def test_out_dir_class(request: FixtureRequest) -> Path:
     """
     Same as tests_out_dir but for class-level fixtures.
+
+    Note: the returned dir is not cleared!
+    For class-level dirs, we can't clear because multiple processes running tests
+    in the same class will remove each-others folders.
     """
-    # For class-level dirs, we can't clear because multiple processes running tests
-    # in the same class will remove each-others folders.
     return _test_out_dir(request, clear=False)
 
 
-def _test_out_dir(request: FixtureRequest, clear: bool = True):
+@pytest.fixture(scope="class")
+def test_out_dir_class_pid(request: FixtureRequest) -> Path:
+    """
+    Same as tests_out_dir but for class-level fixtures.
+    A separate out dir will be created for every test process.
+    It will be cleared if it existed before.
+    """
+    return _test_out_dir(request, clear=True, with_pid=True)
+
+
+def _test_out_dir(request: FixtureRequest, clear: bool = True, with_pid: bool = False):
     filename, *classname_testname = str.split(request.node.nodeid, "::")
     out_dir = TESTS_OUT_DIR.joinpath(*classname_testname)
+    if with_pid:
+        out_dir = out_dir.joinpath(f"pid_{os.getpid()}")
 
     # Clear any previous outputs to prevent any possibility of previous results to
     # affect the current run.
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,10 @@
+import os
+
 import pytest
 
-from experiments.data.mvn import IndependentDataProvider
+from tests import TESTS_OUT_DIR
+from experiments.data.mvn import LinearMVNDataProvider, IndependentDataProvider
+from experiments.data.quantile import QuantileFunctionDataProviderWrapper
 
 
 class TestMVNData(object):
@@ -12,3 +16,75 @@ class TestMVNData(object):
 
         assert X.shape == (n, k)
         assert Y.shape == (n, d)
+
+
+class TestQuantileFunctionDataProviderWrapper:
+    @pytest.fixture(autouse=True)
+    def setup(self, test_out_dir):
+        self.wrapped_provider = LinearMVNDataProvider(k=3, d=1)
+        self.cache_dir = test_out_dir
+
+    @pytest.fixture(autouse=True, scope="class")
+    def providers(self, test_out_dir_class_pid):
+        wrapped_provider = LinearMVNDataProvider(k=3, d=1)
+        cache_dir = test_out_dir_class_pid
+
+        vqr_n_levels = 25
+        vqr_fit_n = 10000
+
+        dp1 = QuantileFunctionDataProviderWrapper(
+            wrapped_provider=wrapped_provider,
+            vqr_solver_opts=dict(verbose=True),
+            vqr_n_levels=vqr_n_levels,
+            vqr_fit_n=vqr_fit_n,
+            cache_dir=cache_dir,
+        )
+
+        dp1_cached = QuantileFunctionDataProviderWrapper(
+            wrapped_provider=wrapped_provider,
+            vqr_solver_opts=dict(verbose=True),
+            vqr_n_levels=vqr_n_levels,
+            vqr_fit_n=vqr_fit_n,
+            cache_dir=cache_dir,
+        )
+
+        dp2 = QuantileFunctionDataProviderWrapper(
+            wrapped_provider=wrapped_provider,
+            vqr_solver_opts=dict(verbose=True),
+            vqr_n_levels=vqr_n_levels // 2,
+            vqr_fit_n=vqr_fit_n // 2,
+            cache_dir=cache_dir,
+        )
+
+        return wrapped_provider, dp1, dp1_cached, dp2
+
+    def test_cache(self, providers):
+        wrapped_provider, dp1, dp1_cached, dp2 = providers
+
+        assert not dp1.cache_loaded
+        assert dp1.cache_saved
+        assert dp1_cached.cache_loaded
+        assert not dp1_cached.cache_saved
+
+        assert not dp2.cache_loaded
+        assert dp2.cache_saved
+
+        assert dp1.cached_path == dp1_cached.cached_path
+        assert dp1.cached_path != dp2
+
+    def test_wrapped_shapes(self, providers):
+        wrapped_provider, dp1, dp1_cached, dp2 = providers
+
+        for dp in [dp1, dp1_cached, dp2]:
+            assert dp.k == wrapped_provider.k
+            assert dp.d == wrapped_provider.d
+
+            n = 100
+
+            for x in [None, dp.sample_x(n=1)]:
+                if x is not None:
+                    assert x.shape == (1, dp.k)
+
+                X, Y = dp1.sample(n=n, x=x)
+                assert Y.shape == (n, dp.d)
+                assert X.shape == (n, dp.k)
