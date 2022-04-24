6ec6e6932d2ea8978dda0429a9afafb77035c494
Sanketh Vedula <sankethsai97@gmail.com>
2022-04-24 11:42:11 +0300

    Widen the dataset along y-axis.

--- experiments/data/synthetic_glasses.py
+++ experiments/data/synthetic_glasses.py
@@ -20,12 +20,12 @@ class SyntheticGlassesDataProvider(DataProvider):
         Z = np.random.rand(n) > 0.5
         Y_i = (
             5 * np.sin(np.linspace(start=0.0, stop=3 * np.pi, num=n))
-            + 0.5
-            + np.random.beta(a=0.5, b=1, size=(n,))
+            + 2.5
+            + 2 * np.random.beta(a=0.5, b=1, size=(n,))
         )
         Y_j = (
             5 * np.sin(np.linspace(start=np.pi, stop=4 * np.pi, num=n))
-            + 0.5
+            + 2.5
             - 2 * np.random.beta(a=0.5, b=1, size=(n,))
         )
         return np.linspace(0, 1, num=n), Z * Y_i + (~Z) * Y_j
@@ -33,6 +33,6 @@ class SyntheticGlassesDataProvider(DataProvider):
 
 if __name__ == "__main__":
     dp = SyntheticGlassesDataProvider()
-    X_, Y_ = dp.sample(n=20000)
+    X_, Y_ = dp.sample(n=3000)
     plt.plot(X_, Y_, ".")
     plt.show()
