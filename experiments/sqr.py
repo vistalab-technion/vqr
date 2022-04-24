838891f43a381511897dde11affd9360c890abaf
Sanketh Vedula <sankethsai97@gmail.com>
2022-04-24 11:42:41 +0300

    Refine the quantiles by solving another small OT problem.

--- experiments/sqr.py
+++ experiments/sqr.py
@@ -1,3 +1,6 @@
+import pickle
+
+import ot
 import numpy as np
 import torch
 from torch import tensor
@@ -5,6 +8,7 @@ from matplotlib import cm
 from matplotlib import pyplot as plt
 
 from vqr import VectorQuantileRegressor
+from vqr.solvers.primal.cvx import SIMILARITY_FN_INNER_PROD
 from experiments.utils.metrics import kde
 from vqr.solvers.dual.regularized_lse import (
     RegularizedDualVQRSolver,
@@ -12,17 +16,17 @@ from vqr.solvers.dual.regularized_lse import (
 )
 from experiments.data.synthetic_glasses import SyntheticGlassesDataProvider
 
-n = 20000
+n = 5000
 d = 1
 k = 1
-T = 1000
-num_epochs = 50000
+T = 100
+num_epochs = 40000
 linear = False
 sigma = 0.1
 GPU_DEVICE_NUM = 1
 device = f"cuda:{GPU_DEVICE_NUM}" if GPU_DEVICE_NUM is not None else "cpu"
 dtype = torch.float32
-epsilon = 1e-9
+epsilon = 1e-3
 X, Y = SyntheticGlassesDataProvider().sample(n)
 
 
@@ -57,41 +61,29 @@ else:
 vqr_est = VectorQuantileRegressor(n_levels=T, solver=solver)
 
 vqr_est.fit(X.reshape(-1, 1), Y.reshape(-1, 1))
+levels = vqr_est.quantile_levels
+quantiles = []
+for i in range(n):
+    X_i = np.array([X[i]])[:, None]
+    quantile_est = vqr_est.vector_quantiles(X=X_i)[0][0]
+    cost = -SIMILARITY_FN_INNER_PROD(levels[:, None], quantile_est[None, :])
+    pi = ot.emd([], [], cost)
+    refined_quantiles = T * pi @ quantile_est
+    quantiles.append(refined_quantiles)
+
+quantiles = np.stack(quantiles, axis=0)
 Y_est = np.concatenate(
     [vqr_est.sample(n=1, x=np.array([X[i]])[:, None])[0] for i in range(n)]
 )
-kde_1 = kde(
-    tensor(np.stack([X, Y_est]), dtype=torch.float32).T,
-    grid_resolution=T,
-    device=device,
-    sigma=sigma,
-)
-plt.figure()
-plt.imshow(
-    kde_1.T,
-    interpolation="bilinear",
-    origin="lower",
-    cmap=cm.RdPu,
-    extent=(0, 1, 0, 1),
-)
-plt.show()
-
-kde_gt = kde(
-    tensor(np.stack([X, Y]), dtype=torch.float32).T,
-    grid_resolution=T,
-    device=device,
-    sigma=sigma,
-)
-plt.figure()
-plt.imshow(
-    kde_gt.T,
-    interpolation="bilinear",
-    origin="lower",
-    cmap=cm.RdPu,
-    extent=(0, 1, 0, 1),
-)
-plt.show()
 
 plt.figure()
 plt.plot(X, Y_est, ".")
-plt.show()
+for quantile_level in np.linspace(0.1, 0.90, 10):
+    plt.plot(X, quantiles[:, int(quantile_level * T)], label=f"{quantile_level:.2f}")
+plt.legend()
+plt.savefig(f"quantile_levels_{linear=}.png")
+
+with open(f"quantiles_{linear=}.pkl", "wb") as f:
+    pickle.dump({"quantiles": quantiles}, f)
+
+exit(0)
