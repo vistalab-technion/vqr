5938d75691a07ad57f3f8047f9aa378ee5370540
Sanketh Vedula <sankethsai97@gmail.com>
2022-04-25 08:46:17 +0300

    POTVQESolver: Compare against CVXVQRSolver for d=1,2,3.

--- tests/test_vqe.py
+++ tests/test_vqe.py
@@ -10,11 +10,23 @@ class TestPOTVQE:
     # Important that N is odd
     @pytest.mark.parametrize("N", [101, 201, 301, 1001])
     @pytest.mark.parametrize("T", [10, 20])
-    def test_pot_quantile_vs_sorted_Y(self, N, T):
+    def test_pot_quantile_vs_sorted_Y_1d(self, N, T):
         Y = np.random.randn(N, 1)
         est_quantiles = POTVQESolver().solve_vqr(T=T, Y=Y).vector_quantiles()[0][0]
         sorted_Y = np.sort(Y.squeeze())[:: int(N / T)]
         assert np.allclose(est_quantiles[1:], sorted_Y[1:-1])
+
+    @pytest.mark.parametrize("N", [101, 201, 301, 1001])
+    @pytest.mark.parametrize("T", [10, 20])
+    @pytest.mark.parametrize("d", [1, 2, 3])
+    def test_pot_quantile_vs_sorted_Y_2d(self, N, T, d):
+        Y = np.random.randn(N, d)
+        est_quantiles = POTVQESolver().solve_vqr(T=T, Y=Y).vector_quantiles()[0]
+        est_quantiles_cvx = CVXVQRSolver().solve_vqr(T=T, Y=Y).vector_quantiles()[0]
+        for Q, Q_cvx in zip(est_quantiles, est_quantiles_cvx):
+            assert Q.shape == (T,) * d
+            assert Q_cvx.shape == (T,) * d
+            assert np.allclose(Q, Q_cvx)
 
     @pytest.mark.parametrize("T", [10, 20, 50, 100])
     @pytest.mark.parametrize("N", [101, 201, 301, 1001])
