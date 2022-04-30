1a24d388f535d900d01b830542de1f861b23a4b1
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-04-30 13:42:38 +0300

    Optimization exp: Refactor to use QuantileFunction

--- experiments/optimization.py
+++ experiments/optimization.py
@@ -1,12 +1,41 @@
+import logging
 from typing import Any, Dict, Optional, Sequence
 
 import click
 import numpy as np
 
-from vqr import VectorQuantileRegressor
+from vqr import QuantileFunction, VectorQuantileRegressor
 from experiments.base import VQROptions, run_exp_context
 from experiments.data.mvn import LinearMVNDataProvider
 from experiments.data.quantile import QuantileFunctionDataProviderWrapper
+
+_LOG = logging.getLogger(__name__)
+
+
+def _compare_conditional_quantiles(
+    vqf_gt: QuantileFunction, vqf_est: QuantileFunction, t_factor: int
+) -> float:
+
+    # Make sure shapes are consistent and that both quantile functions are
+    # conditional on the same X.
+    assert vqf_gt.d == vqf_est.d
+    assert t_factor == vqf_gt.T // vqf_est.T
+    assert np.allclose(vqf_gt.X, vqf_est.X)
+
+    T = vqf_est.T
+    d = vqf_est.d
+
+    q_surfaces_gt = vqf_gt.values  # (d, T, T, ..., T)
+
+    # We support having more levels in the gt function, in which case we index the
+    # levels corresponding to the estimated levels.
+    idx = (slice(None), *[slice(0, None, t_factor)] * d)
+    q_surfaces_gt_subsampled = q_surfaces_gt[idx]
+
+    q_surfaces_est = vqf_est.values  # (d, T, T, ..., T)
+    assert q_surfaces_gt_subsampled.shape == q_surfaces_est.shape
+
+    return np.linalg.norm(q_surfaces_gt_subsampled - q_surfaces_est).item()
 
 
 def single_optim_exp(
@@ -16,26 +45,27 @@ def single_optim_exp(
     k: int,
     solver_name: str,
     solver_opts: dict,
+    dp_vqr_n: int,
+    dp_vqr_t_factor: int,
+    dp_vqr_solver_opts: dict,
+    n_eval_x: int,
     seed: int = 42,
 ) -> Dict[str, Any]:
 
     # Data provider
-    t_factor = 100
     wrapped_provider = LinearMVNDataProvider(d=d, k=k, seed=seed)
     data_provider = QuantileFunctionDataProviderWrapper(
         wrapped_provider=wrapped_provider,
-        vqr_n_levels=T * t_factor,
-        vqr_fit_n=10000,
+        vqr_n_levels=T * dp_vqr_t_factor,
+        vqr_fit_n=dp_vqr_n,
+        vqr_solver_opts=dp_vqr_solver_opts,
     )
 
-    # Obtain g.t. VQR quantile function for x=1
-    x_eval = np.ones((2, k))
-    Qs_opt = data_provider.vqr.fitted_solution.vector_quantiles(X=x_eval)[0]
-    Q_opt = np.stack(Qs_opt, axis=-1)
+    # Sample values of x on which we evaluate
+    eval_x = wrapped_provider.sample_x(n=n_eval_x)
 
-    idx = tuple([*[slice(0, None, t_factor)] * d, slice(None)])
-    Q_opt_subsampled = Q_opt[idx]
-    assert Q_opt_subsampled.shape == tuple([*[T] * d, 1])
+    # Obtain g.t. VQR quantile functions
+    vqfs_gt = data_provider.vqr.fitted_solution.vector_quantiles(X=eval_x)
 
     # Generate data
     X, Y = data_provider.sample(n=N)
@@ -46,21 +76,15 @@ def single_optim_exp(
     def _post_iter_callback(
         solution, batch_loss, epoch_loss, epoch_idx, batch_idx, num_epochs, num_batches
     ):
-        Qs_est = solution.vector_quantiles(X=x_eval)[0]
-        Q_est = np.stack(Qs_est, axis=-1)
+        # Obtain quantile functions from current iteration, conditioned on the same X's
+        vqfs_est = solution.vector_quantiles(X=eval_x)
 
         # Calculate distance from g.t.
-        dist = np.linalg.norm(Q_est - Q_opt_subsampled)
-        optimization_dists.append(dist.item())
-
-        if epoch_idx % 100 == 0:
-            import matplotlib.pyplot as plt
-
-            plt.plot(Q_opt_subsampled, label="opt")
-            plt.plot(Q_est, label="est")
-            plt.title(f"{epoch_idx=}")
-            plt.legend()
-            plt.show()
+        dists = [
+            _compare_conditional_quantiles(vqf_gt, vqf_est, t_factor=dp_vqr_t_factor)
+            for vqf_gt, vqf_est in zip(vqfs_gt, vqfs_est)
+        ]
+        optimization_dists.append(dists)
 
     # Add a solver callback to evaluate the distance from g.t. solution
     solver_opts["post_iter_callback"] = _post_iter_callback
@@ -92,8 +116,32 @@ def single_optim_exp(
 @click.command(name="optim-exp")
 @click.pass_context
 @VQROptions.cli
+@click.option(
+    "--dp-vqr-n",
+    type=int,
+    default=10000,
+    help="Number of samples to use for fitting the data provider's VQR model",
+)
+@click.option(
+    "--dp-t-factor",
+    type=int,
+    default=100,
+    help=(
+        "Factor between data provider's number of quantile levels and the fitted "
+        "models' number of levels. Data provider will have T*t_factor levels."
+    ),
+)
+@click.option(
+    "--n-eval-x",
+    type=int,
+    default=10,
+    help=("Number of X's to use when comparing g.t. to estimated quantile function"),
+)
 def optim_exp(
     ctx: click.Context,
+    dp_vqr_n: int,
+    dp_t_factor: int,
+    n_eval_x: int,
     **kw,
 ):
 
@@ -101,6 +149,10 @@ def optim_exp(
     vqr_options = VQROptions.parse(ctx)
     exp_configs = {
         vqr_option.key(): dict(
+            dp_vqr_n=dp_vqr_n,
+            dp_vqr_t_factor=dp_t_factor,
+            n_eval_x=n_eval_x,
+            dp_vqr_solver_opts=dict(epsilon=5e-3),
             **vqr_option.to_dict(),
         )
         for vqr_option in vqr_options
