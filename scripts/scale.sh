ca8899495c56ce4d2a35f5b943b6605a4e4d17d9
Sanketh Vedula <sankethsai97@gmail.com>
2022-07-26 12:35:44 +0300

    Scale: T

--- scripts/scale.sh
+++ scripts/scale.sh
@@ -13,8 +13,8 @@ PPD=1
 COMMON_ARGS="--epochs=$EPOCHS --lr=$LR --lr-max-steps=$LR_MAX_STEPS --lr-patience=$LR_PATIENCE --lr-factor=$LR_FACTOR"
 
 # N
-python main.py -g --ppd $PPD --out-tag="N" scale-exp $COMMON_ARGS -N 4000 -N 8000 -N \
- 16000 -N 32000 -N 64000 -N 128000 -T 50 -d 2 -k 10
+#python main.py -g --ppd $PPD --out-tag="N" scale-exp $COMMON_ARGS -N 4000 -N 8000 -N \
+# 16000 -N 32000 -N 64000 -N 128000 -T 50 -d 2 -k 10
 
 ## d
 #python main.py -g --ppd $PPD scale-exp $COMMON_ARGS --out-tag="d" -N 10000 -T 10 -d 1 -d 2 -d 3 -d 4 -d 5 -k 10
@@ -23,11 +23,12 @@ python main.py -g --ppd $PPD --out-tag="N" scale-exp $COMMON_ARGS -N 4000 -N 800
 #python main.py -g --ppd $PPD scale-exp $COMMON_ARGS --out-tag="k" -N 10000 -T 50 -d 2 -k 1 -k 2 -k 4 -k 8 -k 16 -k 32 -k 64 -k 128 -k 256 -k 512 -k 1024
 #
 ## T
-#python main.py -g --ppd $PPD scale-exp $COMMON_ARGS --out-tag="T" -N 10000 -T 10 -T 20 -T 30 -T 40 -T 50 -T 60 -T 70 -T 80 -T 90 -T 100 -k 2 -d 2
+python main.py -g --ppd $PPD --out-tag="T" scale-exp $COMMON_ARGS -N 10000 -T 10 \
+-T 20 -T 30 -T 40 -T 50 -T 60 -T 70 -T 80 -T 90 -T 100 -k 2 -d 2
 
 # SGD on N
-python main.py -g --ppd $PPD --out-tag="SGD-N" scale-exp $COMMON_ARGS -N \
-50000 -N 100000 -N 1000000 -N 2000000 -T 50 -d 2 -k 10 --bs-y 50000
+#python main.py -g --ppd $PPD --out-tag="SGD-N" scale-exp $COMMON_ARGS -N \
+#50000 -N 100000 -N 1000000 -N 2000000 -T 50 -d 2 -k 10 --bs-y 50000
 
 # SGD on U
 #python main.py -g --ppd $PPD scale-exp $COMMON_ARGS --out-tag="SGD-U" -N 100000 -T 50 -T 60 -T 70 -T 80 -T 90 -T 100 -d 2 -k 10 --bs-u 2500
