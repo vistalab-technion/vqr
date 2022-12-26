2f266d74118619a33c8f0b2662f8b1881e946a77
Aviv Rosenberg <aviv.rosenberg@gmail.com>
2022-12-26 17:45:42 +0200

    Expose new types via package

--- src/vqr/solvers/__init__.py
+++ src/vqr/solvers/__init__.py
@@ -1,7 +1,7 @@
 from vqr.cvqf import DiscreteCVQF
 from vqr.solvers.cvx import CVXVQRSolver
 from vqr.solvers.pot import POTVQESolver
-from vqr.solvers.base import VQRSolver
+from vqr.solvers.base import Solver, VQESolver, VQRSolver, DiscreteSolver
 from vqr.solvers.regularized_lse import (
     RegularizedDualVQRSolver,
     MLPRegularizedDualVQRSolver,
